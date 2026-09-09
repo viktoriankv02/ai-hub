8caded96fec62af09a604b9b198db215597ab10a
Viktoriankv02 <141505746+viktoriankv02@users.noreply.github.com>
2026-09-09 21:48:39 +0300

    feat: export EVM chain health provider

--- agents/multi-chain/index.ts
+++ agents/multi-chain/index.ts
@@ -3,3 +3,4 @@ export * from "./router.js";
 export * from "./drop-hunter-action.js";
 export * from "./drop-hunter-dispatcher.js";
 export * from "./chain-health.js";
+export * from "./evm-chain-health-provider.js";
