e0b07d6e2d79d19824c70346fc067954efa89a86
Viktoriankv02 <141505746+viktoriankv02@users.noreply.github.com>
2026-09-10 19:19:19 +0300

    feat: export multi-chain runtime registry

--- agents/multi-chain/index.ts
+++ agents/multi-chain/index.ts
@@ -5,3 +5,4 @@ export * from "./drop-hunter-dispatcher.js";
 export * from "./chain-health.js";
 export * from "./evm-chain-health-provider.js";
 export * from "./chain-capabilities.js";
+export * from "./runtime-registry.js";
