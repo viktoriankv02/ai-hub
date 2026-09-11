63b2ffc9af449f085ab3389b881053776a53a5c9
Viktoriankv02 <141505746+viktoriankv02@users.noreply.github.com>
2026-09-11 21:46:23 +0300

    feat(drop-hunter): export reward service

--- agents/drop-hunter/index.ts
+++ agents/drop-hunter/index.ts
@@ -30,6 +30,7 @@ export * from "./drop-hunter-agent.js";
 export * from "./reward-analyzer.js";
 export * from "./reward-model.js";
 export * from "./reward-store.js";
+export * from "./reward-service.js";
 export * from "./transaction-preview.js";
 export * from "./wallet-execution.js";
 export * from "./product-store.js";
